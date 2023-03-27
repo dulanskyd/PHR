124279a892ecaea3950df054159c2be325e8296f
Dominik Dulanský <92888227+dulanskyd@users.noreply.github.com>
2023-03-27 22:06:55 +0200

    Delete Run Trough 0.2 directory

--- Run Trough 0.2/Player/Sprite.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-export var player_path : NodePath
-
-var previous_frame_velocity := Vector2(0,0)
-func _ready() -> void:
-		print("Sprite.gd is missing player_path")
-		set_process(false)
